7d42b07c4ac6f032f4619b655a1906f657596658
Brian DesArmo <31257901+briandesarmo@users.noreply.github.com>
2021-01-26 23:54:59 -0500

    Add project files.

---
+++ Awake/Awake.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Awake
+{
+    internal class Awake
+    {
+        private static readonly DateTime StartTime = DateTime.Now;
+        private static readonly DateTime EndTime = StartTime.AddHours(8);
+        private static readonly Random Random = new Random();
+        private static ExecutionState State => ExecutionState.EsContinuous | ExecutionState.EsAwayModeRequired | ExecutionState.EsDisplayRequired | ExecutionState.EsSystemRequired;
+
+        private static void Main()
+        {
+            for (;DateTime.Now < EndTime; Thread.Sleep(Random.Next(6000, 180000)))
+                SetThreadExecutionState(State);
+        }
+
+        [FlagsAttribute]
+        private enum ExecutionState : uint
+        {
+            EsAwayModeRequired = 0x00000040,
+            EsContinuous = 0x80000000,
+            EsDisplayRequired = 0x00000002,
+            EsSystemRequired = 0x00000001
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern ExecutionState SetThreadExecutionState(ExecutionState esFlags);
+    }
+}
